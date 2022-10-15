64d60b85db446198b1316a5906eab97f6a70bac4
TruongNVQ <go624529@gmail.com>
2022-10-15 22:39:34 +0700

    Test 1

---
+++ build/CMakeFiles/TruongNVQ.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TruongNVQ.dir/main.cpp.obj"
+  "CMakeFiles/TruongNVQ.dir/main.cpp.obj.d"
+  "TruongNVQ.exe"
+  "TruongNVQ.exe.manifest"
+  "TruongNVQ.pdb"
+  "libTruongNVQ.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TruongNVQ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
